81927598aa1edaa5f5d80ce0521bec5a1d2a21d7
Prabhakar <prabhakarkondakalla9@gmail.com>
2025-10-11 16:41:54 +0530

    This Prabha Branch Commited message

---
+++ 1.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int Radius = 7;
+    int Area = 3.14 * Radius * Radius;
+    cout << "The Radius of the Circle is " << Area << " ";
+    return 0;
+}
