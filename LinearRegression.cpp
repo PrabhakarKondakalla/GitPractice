00f4ed521e2977a20ae8e4e1b9a7f6a640817d94
Prabhakar <prabhakarkondakalla9@gmail.com>
2025-10-12 12:49:18 +0530

    This is commited message

--- LinearRegression.cpp
+++ LinearRegression.cpp
@@ -1,40 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int BinarySearch(vector<int> &array, int Target, int Left, int Right)
+int LinearSearch(vector<int> &array, int Target)
 {
-    while (Left <= Right)
+    int n = array.size();
+    for (size_t i = 0; i < n; i++)
     {
-        int mid = Left + (Right - Left) / 2;
-        if (array[mid] == Target)
+        if (array[i] == Target)
         {
-            return mid;
-        }
-        else if (array[mid] > Target)
-        {
-            Right = mid - 1;
-        }
-        else
-        {
-            Left = mid + 1;
+            return i;
         }
     }
     return -1;
 }
 int main()
 {
-    vector<int> array = {1, 2, 3, 4, 5, 6};
-    int Target = 3;
-    int Left = 0;
-    int Right = array.size() - 1;
-    int a = BinarySearch(array, Target, Left, Right);
+    vector<int> array = {1, 2, 4, 3, 6, 8, 9, 10};
+    int Target = 4;
+    int a = LinearSearch(array, Target);
     if (a != -1)
     {
         cout << "The Target Element is Found!!";
     }
     else
     {
-        cout << "The Target  Element is NOT Found!!";
+        cout << "The Target Element is NOT Found!!!";
     }
     return 0;
-    
 }
