0c2c188403a9e60b0b79ecdb99f2339b622d43c1
Prabha <prabhakarkondakalla9@gmail.com>
2026-01-08 21:22:08 +0530

    second commmit

--- Sample.c
+++ Sample.c
@@ -1,6 +1,9 @@
-#include<stdio.h>
-int main(){
-    int n=5;
-    printf("The Value of n is %d",n);
+#include <stdio.h>
+int main()
+{
+    int n = 5;
+    int a = 10;
+    printf("The Value of n is %d", n);
+    printf("The Value of a is %d", a);
     return 0;
 }
